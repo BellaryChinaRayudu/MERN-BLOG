ffa9e93c2d81688d794c30645d9951288a1de1f6
China rayudu <bellarychinna78936@gmail.com>
2024-05-29 08:09:47 +0530

    complete sidebar of tthe ddashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Dashprofile from "../components/Dashprofile";
+import Dashsidebar from "../components/Dashsidebar";
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col md:flex-row">
+        <div className="md:w-56">
+          <Dashsidebar />
+        </div>
+      </div>
+      {tab === "profile" && <Dashprofile />}
+    </>
+  );
 }
